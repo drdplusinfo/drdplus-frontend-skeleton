d19f90ef45a69d320f98dbb5cd1f916c5dcd189b
Jaroslav Týc <mail@jaroslavtyc.com>
2018-05-16 08:25:21 +0200

    Licence background image renamed and tested

--- DrdPlus/Tests/FrontendSkeleton/GraphicsTest.php
+++ DrdPlus/Tests/FrontendSkeleton/GraphicsTest.php
@@ -6,8 +6,16 @@ class GraphicsTest extends AbstractContentTest
     /**
      * @test
      */
-    public function Page_has_monochrome_background_image(): void
+    public function Main_page_has_monochrome_background_image(): void
     {
         self::assertFileExists($this->getDocumentRoot() . '/images/main-background.png');
+    }
+
+    /**
+     * @test
+     */
+    public function Licence_page_has_monochrome_background_image(): void
+    {
+        self::assertFileExists($this->getDocumentRoot() . '/images/licence-background.png');
     }
 }
