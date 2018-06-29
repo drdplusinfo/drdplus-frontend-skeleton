385daf2d218ecbeaa7d8d044e6a66221f8696f15
Jaroslav Týc <mail@jaroslavtyc.com>
2018-06-29 17:55:23 +0200

    Removed invalid parameter for version clone

--- DrdPlus/FrontendSkeleton/WebVersionSwitcher.php
+++ DrdPlus/FrontendSkeleton/WebVersionSwitcher.php
@@ -65,7 +65,7 @@ class WebVersionSwitcher extends StrictObject
         }
         $toVersionDir = $this->dirForVersions . '/' . $toVersion;
         if (!\file_exists($toVersionDir)) {
-            $command = 'git clone --branch ' . \escapeshellarg($toVersion) . ' --depth 1 . ' . \escapeshellarg($toVersionDir) . ' 2>&1'
+            $command = 'git clone --branch ' . \escapeshellarg($toVersion) . ' . ' . \escapeshellarg($toVersionDir) . ' 2>&1'
                 . ' && composer --working-dir ' . \escapeshellarg($toVersionDir) . ' install 2>&1';
             \exec($command, $rows, $returnCode);
             if ($returnCode !== 0) {
