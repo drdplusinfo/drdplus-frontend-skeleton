14b769f5ab664820cc653a28138019a12aed711e
Jaroslav Týc <mail@jaroslavtyc.com>
2018-07-17 13:05:29 +0200

    Page cache prefix can be set from outside

--- DrdPlus/FrontendSkeleton/PageCache.php
+++ DrdPlus/FrontendSkeleton/PageCache.php
@@ -6,8 +6,8 @@ namespace DrdPlus\FrontendSkeleton;
 
 class PageCache extends Cache
 {
-    public function __construct(WebVersions $webVersions, Dirs $dirs, bool $isInProduction)
+    public function __construct(WebVersions $webVersions, Dirs $dirs, bool $isInProduction, string $cachePrefix = null)
     {
-        parent::__construct($webVersions, $dirs, $isInProduction, 'page-' . \md5($dirs->getCacheRoot()));
+        parent::__construct($webVersions, $dirs, $isInProduction, $cachePrefix ?? 'page-' . \md5($dirs->getCacheRoot()));
     }
 }
