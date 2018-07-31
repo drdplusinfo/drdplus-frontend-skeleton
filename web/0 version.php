fb76082a89d746b81bd3c8770b7f616f7a679f90
Jaroslav Týc <mail@jaroslavtyc.com>
2018-07-31 09:30:15 +0200

    Test if heading ID matches heading text

--- web/0 version.php
+++ web/0 version.php
@@ -12,11 +12,12 @@
     <?php } ?>
 </div>
 
-<div class="modal fade" id="confirmOwnership" tabindex="-1" role="dialog" aria-labelledby="confirmOwnershipModalLabel" aria-hidden="true">
+<div class="modal fade" id="confirmOwnership" tabindex="-1" role="dialog" aria-labelledby="<?= \Granam\String\StringTools::toSnakeCaseId("Vlastním {$controller->getWebName()}") ?>" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="confirmOwnershipModalLabel">Vlastním <?= $controller->getWebName() ?></h5>
+        <h5 class="modal-title" id="<?= \Granam\String\StringTools::toSnakeCaseId("Vlastním {$controller->getWebName()}") ?>">
+          Vlastním <?= $controller->getWebName() ?></h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
