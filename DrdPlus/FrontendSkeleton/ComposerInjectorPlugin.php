bc4caa29c916c62e7a024ab58ce09c2e6f6a394c
Jaroslav Týc <mail@jaroslavtyc.com>
2018-08-16 08:41:56 +0200

    Assets and shared configs are copied, cache flushed on this skeleton install

--- DrdPlus/FrontendSkeleton/ComposerInjectorPlugin.php
+++ DrdPlus/FrontendSkeleton/ComposerInjectorPlugin.php
@@ -24,10 +24,22 @@ class ComposerInjectorPlugin implements PluginInterface, EventSubscriberInterfac
         ];
     }
 
+    public function activate(Composer $composer, IOInterface $io): void
+    {
+        $this->composer = $composer;
+        $this->io = $io;
+    }
+
     public function plugInSkeleton()
     {
         $documentRoot = $GLOBALS['documentRoot'] ?? getcwd();
+        $this->publishSkeletonImages($documentRoot);
+        $this->publishSkeletonCss($documentRoot);
+        $this->publishSkeletonJs($documentRoot);
+        $this->flushCache($documentRoot);
         $this->addVersionsToAssets($documentRoot);
+        $this->copyGoogleVerification($documentRoot);
+        $this->copyPhpUnitConfig($documentRoot);
     }
 
     private function addVersionsToAssets(string $documentRoot)
@@ -39,9 +51,72 @@ class ComposerInjectorPlugin implements PluginInterface, EventSubscriberInterfac
         }
     }
 
-    public function activate(Composer $composer, IOInterface $io): void
+    private function publishSkeletonImages(string $documentRoot)
     {
-        $this->composer = $composer;
-        $this->io = $io;
+        $this->passThrough(
+            [
+                'rm -f ./images/generic/skeleton/frontend*',
+                'cp -r ./vendor/drd-plus/frontend-skeleton/images/generic ./images/'
+            ],
+            $documentRoot
+        );
+    }
+
+    private function passThrough(array $commands, string $workingDir = null): void
+    {
+        if ($workingDir !== null) {
+            $escapedWorkingDir = \escapeshellarg($workingDir);
+            \array_unshift($commands, 'cd ' . $escapedWorkingDir);
+        }
+        foreach ($commands as &$command) {
+            $command .= ' 2>&1';
+        }
+        unset($command);
+        $chain = \implode(' && ', $commands);
+        \passthru($chain, $returnCode);
+        if ($returnCode !== 0) {
+            $this->io->writeError('Failed injecting skeleton by command ' . $chain);
+        }
+    }
+
+    private function publishSkeletonCss(string $documentRoot): void
+    {
+        $this->passThrough(
+            [
+                'rm -f ./css/generic/skeleton/frontend*',
+                'rm -fr ./css/generic/skeleton/vendor/frontend',
+                'cp -r ./vendor/drd-plus/frontend-skeleton/css/generic ./css/',
+                'chmod -R g+w ./css/generic/skeleton/vendor/frontend'
+            ],
+            $documentRoot
+        );
+    }
+
+    private function publishSkeletonJs(string $documentRoot): void
+    {
+        $this->passThrough(
+            [
+                'rm -f ./js/generic/skeleton/frontend*',
+                'rm -fr ./js/generic/skeleton/vendor/frontend',
+                'cp -r ./vendor/drd-plus/frontend-skeleton/js/generic ./js/',
+                'chmod -R g+w ./js/generic/skeleton/vendor/frontend'
+            ],
+            $documentRoot
+        );
+    }
+
+    private function flushCache(string $documentRoot): void
+    {
+        $this->passThrough(['find ./cache -mindepth 2 -type f -exec rm {} +'], $documentRoot);
+    }
+
+    private function copyGoogleVerification(string $documentRoot)
+    {
+        $this->passThrough(['cp ./vendor/drd-plus/frontend-skeleton/google8d8724e0c2818dfc.html .'], $documentRoot);
+    }
+
+    private function copyPhpUnitConfig(string $documentRoot)
+    {
+        $this->passThrough(['cp ./vendor/drd-plus/frontend-skeleton/phpunit.xml.dist .'], $documentRoot);
     }
 }
